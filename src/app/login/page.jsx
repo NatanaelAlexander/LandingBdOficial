1a57212d9b950f066324649afc396da824f3a766
Natanael Muñoz <namuji38mail.com>
2024-08-04 20:39:48 -0400

    Esta listo el login, quite lo de restablecer contraseña, problema para el nata del futuro

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,127 +1,123 @@
-'use client';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+'use client'
+import { useEffect, useState } from "react"
+import axios from "axios";
 import { useRouter } from 'next/navigation';
-
-import { square } from 'ldrs';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const router = useRouter();
     const [loading, setLoading] = useState(false);
-    const [message, setMessage] = useState('');
+    const [message, setMessasge] = useState('');
 
     useEffect(() => {
-        // Solo se ejecuta en el cliente
+        const { square } = require('ldrs');
         square.register();
     }, []);
 
-    const handleSubmit = async (e) => {
+    const handlesubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
 
         try {
             const response = await axios.post('/api/login', { email, password });
-            setMessage(response.data.message);
+            setMessasge(response.data.message);
 
             if (response.status === 200) {
                 console.log('Login successful');
                 const token = response.data.token;
                 localStorage.setItem('token', token);
                 setTimeout(() => {
-                    setMessage('');
-                    router.push('/adminPage');
                     setLoading(false);
+                    setMessasge('');
+                    router.push('/adminPage');
                 }, 1000);
             }
+
         } catch (err) {
             setTimeout(() => {
                 setLoading(false);
-                setMessage(err.response.data.message);
+                setMessasge(err.response.data.message);
                 console.log('error del front: ', err);
             }, 1000);
         }
     };
 
     return (
-        <div className="flex h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
-            {loading ? (
-                <div>
-                    <l-square
-                        size="35"
-                        stroke="5"
-                        stroke-length="0.25"
-                        bg-opacity="0.1"
-                        speed="1.2"
-                        color="white"
-                    ></l-square>
-                </div>
-            ) : (
-                <div>
-                    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <h2 className="mt-10 text-center text-2xl md:text-5xl font-bold leading-9 tracking-tight text-white">
-                            Iniciar sesión
-                        </h2>
+        <>
+            <div className="flex h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
+                {loading ? (
+                    <div>
+                        <l-square
+                            size="35"
+                            stroke="5"
+                            stroke-length="0.25"
+                            bg-opacity="0.1"
+                            speed="1.2"
+                            color="white"
+                        ></l-square>
                     </div>
+                ) : (
+                    <div>
+                        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                            <h2 className="mt-10 text-center text-2xl md:text-5xl font-bold leading-9 tracking-tight text-white">
+                                Iniciar sesión
+                            </h2>
+                        </div>
 
-                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                        <form onSubmit={handleSubmit} method="POST" className="space-y-6">
-                            <div>
-                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
-                                    Correo
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        id="email"
-                                        name="email"
-                                        type="email"
-                                        required
-                                        autoComplete="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        className="block w-full rounded-md border-0 px-2 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    />
-                                </div>
-                            </div>
-
-                            <div>
-                                <div className="flex items-center justify-between">
-                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                                        Contraseña
+                        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                            <form onSubmit={handlesubmit} method="POST" className="space-y-6">
+                                <div>
+                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                                        Correo
                                     </label>
-                                    <div className="text-sm">
-                                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                            Olvidaste la contraseña?
-                                        </a>
+                                    <div className="mt-2">
+                                        <input
+                                            id="email"
+                                            name="email"
+                                            type="email"
+                                            required
+                                            autoComplete="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            className="block w-full rounded-md border-0 px-2 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
                                     </div>
                                 </div>
-                                <div className="mt-2">
-                                    <input
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        required
-                                        autoComplete="current-password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        className="block w-full rounded-md border-0 py-1.5 text-black px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    />
+
+                                <div>
+                                    <div className="flex items-center justify-between">
+                                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                                            Contraseña
+                                        </label>
+                                    </div>
+                                    <div className="mt-2">
+                                        <input
+                                            id="password"
+                                            name="password"
+                                            type="password"
+                                            required
+                                            autoComplete="current-password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            className="block w-full rounded-md border-0 py-1.5 text-black px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="text-red-500 font-semibold">{message}</div>
-                            <div>
-                                <button
-                                    type="submit"
-                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Iniciar sesión
-                                </button>
-                            </div>
-                        </form>
+                                <div className="text-red-500 font-semibold">{message}</div>
+                                <div>
+                                    <button
+                                        type="submit"
+                                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Iniciar sesión
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </>
     );
 }
